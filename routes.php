f865981962a05b8ac8254ad8989c165ba47fdfb0
SamPoyigi <sam@sampoyigi.com>
2018-10-21 17:15:04 +0100

    Add web middleware to payregister entry point routes

--- routes.php
+++ routes.php
@@ -1,9 +1,10 @@
 <?php
 
-Route::prefix('ti_payregister')->group(function () {
-
+Route::group([
+    'prefix' => 'ti_payregister',
+    'middleware' => ['web']
+], function () {
     Route::any('{code}/{slug}', function ($code, $slug) {
-
         return \Admin\Classes\PaymentGateways::runEntryPoint($code, $slug);
     })->where('slug', '(.*)?');
 });
