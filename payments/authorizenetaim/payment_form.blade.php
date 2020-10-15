76e9e340389675f8df26c49d96166a7d32b7e33c
Sam Poyigi <sam@sampoyigi.com>
2020-10-15 22:22:10 +0100

    Use blade templates
    Signed-off-by: Sam Poyigi <sam@sampoyigi.com>

--- payments/authorizenetaim/payment_form.blade.php
+++ payments/authorizenetaim/payment_form.blade.php
@@ -3,38 +3,38 @@
     class="payment-form w-100"
     data-button-selector=".AcceptUI"
     data-error-selector="#authorizenetaim-errors"
-    data-accept-js-endpoint="<?= $paymentMethod->getEndPoint() ?>"
+    data-accept-js-endpoint="{{ $paymentMethod->getEndPoint() }}"
 >
-    <?php foreach ($paymentMethod->getHiddenFields() as $name => $value) { ?>
-        <input type="hidden" name="<?= $name; ?>" value="<?= $value; ?>"/>
-    <?php } ?>
+    @foreach ($paymentMethod->getHiddenFields() as $name => $value)
+        <input type="hidden" name="{{ $name }}" value="{{ $value }}"/>
+    @endforeach
 
-    <?php if ($paymentProfile = $paymentMethod->findPaymentProfile($order->customer)) { ?>
+    @if ($paymentProfile = $paymentMethod->findPaymentProfile($order->customer))
         <div class="form-group">
             <input type="hidden" name="pay_from_profile" value="1">
             <div>
-                <i class="fab fa-fw fa-cc-<?= $paymentProfile->card_brand ?>"></i>&nbsp;&nbsp;
-                <b>************&nbsp;<?= $paymentProfile->card_last4 ?></b>
+                <i class="fab fa-fw fa-cc-{{ $paymentProfile->card_brand }}"></i>&nbsp;&nbsp;
+                <b>************&nbsp;{{ $paymentProfile->card_last4 }}</b>
                 &nbsp;&nbsp;-&nbsp;&nbsp;
                 <a
                     class="text-danger"
                     href="javascript:;"
                     data-checkout-control="delete-payment-profile"
-                    data-payment-code="<?= $paymentMethod->code ?>"
-                ><?= lang('igniter.payregister::default.button_delete_card') ?></a>
+                    data-payment-code="{{ $paymentMethod->code }}"
+                >@lang('igniter.payregister::default.button_delete_card')</a>
             </div>
         </div>
-    <?php } else { ?>
+    @else
         <button
             type="button"
             class="AcceptUI hide"
             data-billingAddressOptions='{"show":true, "required":false}'
-            data-apiLoginID="<?= $paymentMethod->getApiLoginID() ?>"
-            data-clientKey="<?= $paymentMethod->getClientKey() ?>"
+            data-apiLoginID="{{ $paymentMethod->getApiLoginID() }}"
+            data-clientKey="{{ $paymentMethod->getClientKey() }}"
             data-paymentOptions='{"showCreditCard": true, "showBankAccount": false}'
             data-acceptUIFormHeaderTxt="Card Information"
             data-responseHandler="authorizeNetAimResponseHandler"
         ></button>
         <div id="authorizenetaim-errors" class="text-danger"></div>
-    <?php } ?>
+    @endif
 </div>
--- payments/stripe/payment_form.blade.php
+++ payments/stripe/payment_form.blade.php
@@ -1,32 +1,32 @@
 <div
     id="stripePaymentForm"
     class="payment-form w-100"
-    data-publishable-key="<?= $paymentMethod->getPublishableKey() ?>"
+    data-publishable-key="{{ $paymentMethod->getPublishableKey() }}"
     data-card-selector="#stripe-card-element"
     data-error-selector="#stripe-card-errors"
 >
-    <?php foreach ($paymentMethod->getHiddenFields() as $name => $value) { ?>
-        <input type="hidden" name="<?= $name; ?>" value="<?= $value; ?>"/>
-    <?php } ?>
+    @foreach ($paymentMethod->getHiddenFields() as $name => $value)
+        <input type="hidden" name="{{ $name }}" value="{{ $value }}"/>
+    @endforeach
 
     <div class="form-group">
-        <?php if ($paymentProfile = $paymentMethod->findPaymentProfile($order->customer)) { ?>
+        @if ($paymentProfile = $paymentMethod->findPaymentProfile($order->customer))
             <input type="hidden" name="pay_from_profile" value="1">
             <div>
-                <i class="fab fa-fw fa-cc-<?= $paymentProfile->card_brand ?>"></i>&nbsp;&nbsp;
-                <b>&bull;&bull;&bull;&bull;&nbsp;&bull;&bull;&bull;&bull;&nbsp;&bull;&bull;&bull;&bull;&nbsp;<?= $paymentProfile->card_last4 ?></b>
+                <i class="fab fa-fw fa-cc-{{ $paymentProfile->card_brand }}"></i>&nbsp;&nbsp;
+                <b>&bull;&bull;&bull;&bull;&nbsp;&bull;&bull;&bull;&bull;&nbsp;&bull;&bull;&bull;&bull;&nbsp;{{ $paymentProfile->card_last4 }}</b>
                 &nbsp;&nbsp;-&nbsp;&nbsp;
                 <a
                     class="text-danger"
                     href="javascript:;"
                     data-checkout-control="delete-payment-profile"
-                    data-payment-code="<?= $paymentMethod->code ?>"
-                ><?= lang('igniter.payregister::default.button_delete_card') ?></a>
+                    data-payment-code="{{ $paymentMethod->code }}"
+                >@lang('igniter.payregister::default.button_delete_card')</a>
             </div>
-        <?php } else { ?>
+        @else
             <label
                 for="stripe-card-element"
-            ><?= lang('igniter.payregister::default.stripe.text_credit_or_debit') ?></label>
+            >@lang('igniter.payregister::default.stripe.text_credit_or_debit')</label>
             <div id="stripe-card-element">
                 <!-- A Stripe Element will be inserted here. -->
             </div>
@@ -34,7 +34,7 @@
             <!-- Used to display form errors. -->
             <div id="stripe-card-errors" class="text-danger" role="alert"></div>
 
-            <?php if ($paymentMethod->supportsPaymentProfiles() AND $order->customer) { ?>
+            @if ($paymentMethod->supportsPaymentProfiles() AND $order->customer)
                 <div class="custom-control custom-checkbox mt-2">
                     <input
                         id="save-customer-profile"
@@ -46,9 +46,9 @@
                     <label
                         class="custom-control-label"
                         for="save-customer-profile"
-                    ><?= lang('igniter.payregister::default.text_save_card_profile'); ?></label>
+                    >@lang('igniter.payregister::default.text_save_card_profile');</label>
                 </div>
-            <?php } ?>
-        <?php } ?>
+            @endif
+        @endif
     </div>
 </div>
