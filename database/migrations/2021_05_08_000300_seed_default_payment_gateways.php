a0509e053600140886484e7e2d309590341bbd00
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-05-16 21:51:17 +0100

    Minor fix

--- database/migrations/2021_05_08_000300_seed_default_payment_gateways.php
+++ database/migrations/2021_05_08_000300_seed_default_payment_gateways.php
@@ -6,12 +6,16 @@ use Admin\Models\Payments_model;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Main\Classes\ThemeManager;
 
 class SeedDefaultPaymentGateways extends Migration
 {
     public function up()
     {
         if (!Schema::hasTable('payments') OR DB::table('payments')->count())
+            return;
+
+        if (!ThemeManager::instance()->getActiveTheme())
             return;
 
         Payments_model::syncAll();
