b0166ed0ce6795dfb4ad0f894f10d59de415ae4d
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-07-03 23:19:39 +0100

    Refactor location options as registered location settings
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Subscribers/FormFieldsSubscriber.php
+++ src/Subscribers/FormFieldsSubscriber.php
@@ -4,10 +4,6 @@ namespace Igniter\PayRegister\Subscribers;
 
 use Igniter\Admin\Widgets\Form;
 use Igniter\Cart\Models\Order;
-use Igniter\Local\Events\LocationDefineOptionsFieldsEvent;
-use Igniter\Local\Requests\LocationRequest;
-use Igniter\PayRegister\Models\Payment;
-use Igniter\System\Classes\FormRequest;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class FormFieldsSubscriber
@@ -15,42 +11,11 @@ class FormFieldsSubscriber
     public function subscribe(Dispatcher $events): array
     {
         return [
-            LocationDefineOptionsFieldsEvent::class => 'handle',
-            'system.formRequest.extendValidator' => 'handleValidation',
-            'admin.form.extendFieldsBefore' => 'addPaymentLogsField',
+            'admin.form.extendFieldsBefore' => 'handle',
         ];
     }
 
-    public function handle(LocationDefineOptionsFieldsEvent $event): array
-    {
-        return [
-            'payments' => [
-                'label' => 'lang:igniter.payregister::default.label_payments',
-                'accordion' => 'lang:igniter.local::default.text_tab_general_options',
-                'type' => 'checkboxlist',
-                'options' => [Payment::class, 'listDropdownOptions'],
-                'commentAbove' => 'lang:igniter.payregister::default.help_payments',
-                'placeholder' => 'lang:igniter.payregister::default.help_no_payments',
-            ],
-        ];
-    }
-
-    public function handleValidation(FormRequest $formRequest, object $dataHolder)
-    {
-        if (!$formRequest instanceof LocationRequest) {
-            return;
-        }
-
-        $dataHolder->attributes = array_merge($dataHolder->attributes, [
-            'options.payments.*' => lang('igniter.payregister::default.label_payments'),
-        ]);
-
-        $dataHolder->rules = array_merge($dataHolder->rules, [
-            'options.payments.*' => ['string'],
-        ]);
-    }
-
-    public function addPaymentLogsField(Form $form)
+    public function handle(Form $form)
     {
         if ($form->model instanceof Order) {
             $form->tabs['fields']['payment_logs'] = [
