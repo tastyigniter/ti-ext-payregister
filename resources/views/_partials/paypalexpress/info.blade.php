d367b940a8d018d68621a6d83d79e4b3686a13b1
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-07-18 17:03:24 +0100

    Refactored CSS code, classes, template files to improve admin dashboard UI
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- resources/views/_partials/paypalexpress/info.blade.php
+++ resources/views/_partials/paypalexpress/info.blade.php
@@ -1,14 +1,22 @@
-<h5>To obtain your Live API credentials.</h5>
-<ul class="list-group list-group-flush rounded">
-    <li class="list-group-item">From your PayPal business account.</li>
-    <li class="list-group-item">
-        You can obtain an API Access credential in one of two ways:
-        <ol>
-            <li>Select API Access from the Activity menu at the top of the screen,</li>
-            <li>or from Account preferences > API Access.</li>
-        </ol>
-    </li>
-    <li class="list-group-item">Scroll down and choose Manage API credentials under NVP/SOAP API Integration (Classic).</li>
-    <li class="list-group-item">If you haven't previously, create keys. Otherwise, duplicate the API Username, API Password, and Signature.</li>
-    <li class="list-group-item">Paste the API Username, API Password, and Signature into the plugin's corresponding fields.</li>
-</ul>
+<div class="bg-light mt-3 p-3 border rounded">
+    <h5>To obtain your Live API credentials.</h5>
+    <ol class="pl-3">
+        <li>From your PayPal business account.</li>
+        <li>
+            You can obtain an API Access credential in one of two ways:
+            <ol type="a">
+                <li>Select API Access from the Activity menu at the top of the screen,</li>
+                <li>or from Account preferences > API Access.</li>
+            </ol>
+        </li>
+        <li>Scroll down and choose Manage API credentials under NVP/SOAP API Integration
+            (Classic).
+        </li>
+        <li>If you haven't previously, create keys. Otherwise, duplicate the API Username, API
+            Password, and Signature.
+        </li>
+        <li>Paste the API Username, API Password, and Signature into the plugin's corresponding
+            fields.
+        </li>
+    </ol>
+</div>
