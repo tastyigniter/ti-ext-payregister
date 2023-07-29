42cbd49e80855de909ad436db81bef7fb7ace6e0
sampoyigi <sampoyigi@users.noreply.github.com>
2023-07-29 20:20:58 +0000

    PHP Linting (Pint) - Fix styling

--- database/migrations/2021_05_08_000300_seed_default_payment_gateways.php
+++ database/migrations/2021_05_08_000300_seed_default_payment_gateways.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up()
     {
         if (!Schema::hasTable('payments') || DB::table('payments')->count()) {
