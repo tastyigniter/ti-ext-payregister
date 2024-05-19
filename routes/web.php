dd046a401c050a3c5907a2017d92c86c4ca27edc
sampoyigi <sampoyigi@users.noreply.github.com>
2024-05-19 22:48:03 +0000

    PHP Linting (Pint) - Fix styling

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 Route::group([
     'prefix' => 'ti_payregister',
     'middleware' => ['web'],
-], function () {
-    Route::any('{code}/{slug}', function ($code, $slug) {
+], function() {
+    Route::any('{code}/{slug}', function($code, $slug) {
         return \Igniter\PayRegister\Classes\PaymentGateways::runEntryPoint($code, $slug);
     })->where('slug', '(.*)?');
 });
