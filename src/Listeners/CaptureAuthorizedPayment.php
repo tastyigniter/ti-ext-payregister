0ac3b69828757328924ae2d5eb6e04b6dd25c9a7
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-05-23 15:08:12 +0100

    Code refactor
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Listeners/CaptureAuthorizedPayment.php
+++ src/Listeners/CaptureAuthorizedPayment.php
@@ -5,21 +5,15 @@ namespace Igniter\PayRegister\Listeners;
 use Igniter\Admin\Models\StatusHistory;
 use Igniter\Cart\Models\Order;
 use Igniter\Flame\Database\Model;
-use Igniter\Flame\Exception\ApplicationException;
 use Igniter\PayRegister\Concerns\WithAuthorizedPayment;
 
 class CaptureAuthorizedPayment
 {
     public function handle(Model $order, StatusHistory $statusHistory)
     {
-        if (!$order instanceof Order) {
+        if (!$order instanceof Order || !$paymentMethod = $order->payment_method) {
             return;
         }
-
-        throw_unless(
-            $paymentMethod = $order->payment_method,
-            new ApplicationException('No valid payment method found')
-        );
 
         if (!in_array(WithAuthorizedPayment::class, class_uses($paymentMethod->getGatewayClass()))) {
             return;
