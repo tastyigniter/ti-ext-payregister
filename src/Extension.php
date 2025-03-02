sampoyigi/ti-ext-telescope

Regroup permissions
Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

src/Extension.php
@@ -51,7 +51,7 @@ class Extension extends BaseExtension
     {
         return [
             'SamPoyigi.Telescope.Access' => [
-                'tab' => 'Telescope',
+                'group' => 'advanced',
                 'label' => 'Access to the Telescope dashboard',
             ],
         ];

tastyigniter/ti-ext-payregister

src/Extension.php
@@ -99,7 +99,7 @@ class Extension extends BaseExtension
         return [
             'Admin.Payments' => [
                 'label' => 'igniter.payregister::default.help_permission',
-                'group' => 'sales',
+                'group' => 'order',
             ],
         ];
     }